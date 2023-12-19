7af1f49837b3c8a2cdba07993fabe9cacea645c3
Jim Gustafson <jim@signal.org>
2023-12-19 14:55:32 -0800

    Change the cleaner to work concurrently

--- frontend/src/cleaner.rs
+++ frontend/src/cleaner.rs
@@ -5,10 +5,14 @@
 
 use anyhow::Result;
 use calling_common::Duration;
+use futures::future::join_all;
 use log::*;
+use parking_lot::Mutex;
 use rand::{thread_rng, Rng};
-use tokio::sync::oneshot::Receiver;
+use std::sync::Arc;
+use tokio::sync::{oneshot::Receiver, Semaphore};
 
+use crate::storage::CallRecordKey;
 use crate::{
     backend::{self, Backend, BackendError, BackendHttpClient},
     config,
@@ -31,14 +35,11 @@ async fn does_call_exist_on_backend(call_record: &CallRecord, backend: &BackendH
                 .get_clients(&backend_address, &call_record.era_id, None)
                 .await
             {
-                match err {
-                    BackendError::CallNotFound => {
-                        event!("calling.frontend.cleaner.get.clients.call_not_found");
-                    }
-                    _ => {
-                        error!("failed to get clients from backend: {:?}", err);
-                        event!("calling.frontend.cleaner.get.clients.backend_error");
-                    }
+                if let BackendError::CallNotFound = err {
+                    event!("calling.frontend.cleaner.get.clients.call_not_found");
+                } else {
+                    error!("failed to get clients from backend: {:?}", err);
+                    event!("calling.frontend.cleaner.get.clients.backend_error");
                 }
 
                 // The Calling Backend is not handling the call or there was an unexpected error.
@@ -54,8 +55,8 @@ async fn does_call_exist_on_backend(call_record: &CallRecord, backend: &BackendH
 pub async fn start(config: &'static config::Config, ender_rx: Receiver<()>) -> Result<()> {
     let cleanup_interval = Duration::from_millis(config.cleanup_interval_ms);
 
-    let storage = Box::new(DynamoDb::new(config).await?);
-    let backend = Box::new(BackendHttpClient::from_config(config).await?);
+    let storage = Arc::new(DynamoDb::new(config).await?);
+    let backend = Arc::new(BackendHttpClient::from_config(config).await?);
 
     // Spawn a normal (cooperative) task to cleanup calls from storage periodically.
     let cleaner_handle = tokio::spawn(async move {
@@ -71,24 +72,59 @@ pub async fn start(config: &'static config::Config, ender_rx: Receiver<()>) -> R
             let cleaner_timer = start_timer_us!("calling.frontend.cleaner.timed");
 
             match storage.get_call_records_for_region(&config.region).await {
-                Ok(calls) => {
-                    for call_record in calls {
-                        if !does_call_exist_on_backend(&call_record, &backend).await {
-                            info!(
-                                "Cleaning up call: {} - {:.6}",
-                                call_record.room_id, call_record.era_id
-                            );
+                Ok(calls) if !calls.is_empty() => {
+                    debug!("Found {} calls in table!", calls.len());
 
-                            // Remove the call from the database since it doesn't exist anymore
-                            // on the Calling Backend (or there is an *error* accessing it).
-                            if let Err(err) = storage
-                                .remove_call_record(&call_record.room_id, &call_record.era_id)
-                                .await
+                    let calls_to_remove = Arc::new(Mutex::new(Vec::new()));
+
+                    // Create a list of handles for requests to the backend, limited by semaphore to 10 at a time.
+                    let mut join_handles = vec![];
+                    let permits = Arc::new(Semaphore::new(10));
+                    for call_record in calls {
+                        let backend = Arc::clone(&backend);
+                        let calls_to_remove = Arc::clone(&calls_to_remove);
+                        let permits = permits.clone();
+
+                        join_handles.push(async move {
+                            let permit = permits.acquire().await;
+                            if permit.is_ok()
+                                && !does_call_exist_on_backend(&call_record, &backend).await
                             {
-                                error!("{:?}", err);
+                                calls_to_remove.lock().push(CallRecordKey {
+                                    room_id: call_record.room_id,
+                                    era_id: call_record.era_id,
+                                });
                             }
+                        });
+                    }
+                    join_all(join_handles).await;
+
+                    let calls_to_remove = {
+                        let mut guard = calls_to_remove.lock();
+                        let calls: &mut Vec<_> = &mut guard;
+                        std::mem::take(calls)
+                    };
+                    debug!("Found {} calls to cleanup!", calls_to_remove.len());
+
+                    for call_record_key in &calls_to_remove {
+                        info!(
+                            "Cleaning up call: {} - {:.6}",
+                            call_record_key.room_id, call_record_key.era_id
+                        );
+                    }
+
+                    for chunk in calls_to_remove.chunks(100) {
+                        debug!("Batch removing {} calls.", chunk.len());
+
+                        // Remove the calls from the database since they don't exist anymore
+                        // on a Calling Backend (or there was an *error* accessing it).
+                        if let Err(err) = storage.remove_batch_call_records(chunk.to_vec()).await {
+                            error!("{:?}", err);
                         }
                     }
+                }
+                Ok(_) => {
+                    debug!("Found 0 calls in table!");
                 }
                 Err(err) => {
                     error!("{:?}", err);
@@ -109,4 +145,37 @@ pub async fn start(config: &'static config::Config, ender_rx: Receiver<()>) -> R
 
     info!("cleaner shutdown");
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use calling_common::{random_hex_string, RoomId};
+
+    use crate::config::default_test_config;
+
+    use super::*;
+
+    // Run this test manually to load several call records into local storage.
+    // 1. Run the docker compose environment
+    // 2. Run this test function
+    // 3. Watch for the cleaner results
+    #[tokio::test]
+    #[ignore]
+    async fn test_inject_random_records() -> Result<()> {
+        let storage = DynamoDb::new(&default_test_config()).await?;
+
+        for _ in 0..1000 {
+            let call_record = CallRecord {
+                room_id: RoomId::from(random_hex_string(32)),
+                era_id: random_hex_string(32),
+                backend_ip: "192.168.0.126".to_string(),
+                backend_region: "us-west1".to_string(),
+                creator: random_hex_string(32),
+            };
+
+            let _ = storage.get_or_add_call_record(call_record).await?;
+        }
+
+        Ok(())
+    }
 }
