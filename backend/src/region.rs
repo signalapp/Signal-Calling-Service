0b4b5878180c900b2837072ce1a29c98a0e6fa37
Richard Russo <richard@signal.org>
2025-01-06 13:36:43 -0800

    Add more GCP regions and locations

--- backend/src/region.rs
+++ backend/src/region.rs
@@ -12,25 +12,41 @@ use strum_macros::{Display, EnumIter, EnumString};
 pub enum Region {
     Unset,
     Unknown,
+    AfricaSouth1,
     AsiaEast1,
     AsiaEast2,
     AsiaNortheast1,
+    AsiaNortheast2,
     AsiaNortheast3,
     AsiaSouth1,
+    AsiaSouth2,
     AsiaSoutheast1,
     AsiaSoutheast2,
-    AustralianSoutheast2,
+    AustraliaSoutheast1,
+    AustraliaSoutheast2,
+    EuropeCentral2,
     EuropeNorth1,
+    EuropeSouthwest1,
     EuropeWest1,
     EuropeWest2,
     EuropeWest3,
     EuropeWest4,
     EuropeWest6,
+    EuropeWest8,
+    EuropeWest9,
+    EuropeWest12,
+    MeCentral1,
+    MeCentral2,
+    MeWest1,
     NorthamericaNortheast1,
+    NorthamericaNortheast2,
+    NorthamericaSouth1,
     SouthamericaEast1,
+    SouthamericaWest1,
     UsCentral1,
     UsEast1,
     UsEast4,
+    UsSouth1,
     UsWest1,
     UsWest2,
     UsWest3,
@@ -40,38 +56,57 @@ pub enum Region {
 // Google sometimes calls this "location"
 #[derive(PartialEq)]
 enum GlobalArea {
+    Africa,
     AsiaPacific,
+    Australia, // GCP considers Australia part of AsiaPacific
     Europe,
-    NorthAmerica,
-    SouthAmerica,
+    NorthAmerica, // GCP considers NorthAmerica part of Americas
+    MiddleEast,
+    SouthAmerica, // GCP considers SouthAmerica part of Americas
 }
 
 impl Region {
     fn area(&self) -> Option<GlobalArea> {
         match self {
+            Region::AfricaSouth1 => Some(GlobalArea::Africa),
             Region::AsiaEast1
             | Region::AsiaEast2
             | Region::AsiaNortheast1
+            | Region::AsiaNortheast2
             | Region::AsiaNortheast3
             | Region::AsiaSouth1
+            | Region::AsiaSouth2
             | Region::AsiaSoutheast1
-            | Region::AsiaSoutheast2
-            | Region::AustralianSoutheast2 => Some(GlobalArea::AsiaPacific),
-            Region::EuropeNorth1
+            | Region::AsiaSoutheast2 => Some(GlobalArea::AsiaPacific),
+            Region::AustraliaSoutheast1 | Region::AustraliaSoutheast2 => {
+                Some(GlobalArea::Australia)
+            }
+            Region::EuropeCentral2
+            | Region::EuropeNorth1
+            | Region::EuropeSouthwest1
             | Region::EuropeWest1
             | Region::EuropeWest2
             | Region::EuropeWest3
             | Region::EuropeWest4
-            | Region::EuropeWest6 => Some(GlobalArea::Europe),
+            | Region::EuropeWest6
+            | Region::EuropeWest8
+            | Region::EuropeWest9
+            | Region::EuropeWest12 => Some(GlobalArea::Europe),
+            Region::MeCentral1 | Region::MeCentral2 | Region::MeWest1 => {
+                Some(GlobalArea::MiddleEast)
+            }
             Region::NorthamericaNortheast1
+            | Region::NorthamericaNortheast2
+            | Region::NorthamericaSouth1
             | Region::UsCentral1
             | Region::UsEast1
             | Region::UsEast4
+            | Region::UsSouth1
             | Region::UsWest1
             | Region::UsWest2
             | Region::UsWest3
             | Region::UsWest4 => Some(GlobalArea::NorthAmerica),
-            Region::SouthamericaEast1 => Some(GlobalArea::SouthAmerica),
+            Region::SouthamericaEast1 | Region::SouthamericaWest1 => Some(GlobalArea::SouthAmerica),
             Region::Unset | Region::Unknown => None,
         }
     }
